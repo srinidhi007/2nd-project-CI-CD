92c7c450d7a32233b57217b24419c8cbcd9d7db6
srinidhi007 <chintham.srinidhi@gmail.com>
2023-06-01 16:59:17 +0300

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flask-ml-service-sri.azurewebsites.net:$PORT/predict 
+     -X POST https://flask-ml-azure-sri.azurewebsites.net:$PORT/predict
      #your application name <yourappname>goes here
