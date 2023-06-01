0bfbf1c33e6063ebce58394f7aa5c9c0f8abafc3
srinidhi007 <chintham.srinidhi@gmail.com>
2023-06-01 19:40:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home from azure</h3>"
+    html = "<h3>Sklearn Prediction Home from azure from srinidhi</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
